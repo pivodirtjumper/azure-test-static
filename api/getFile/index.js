a8925e8389625bea60abecf00ac1b8766dc9c51f
Bohdan Pyvovar <bohdanpyvovar@MacBook-Pro-Bohdan.local>
2021-04-08 12:33:56 +0300

    Moved functions

---
+++ api/getFile/index.js
@@ -0,0 +1,40 @@
+const { BlobServiceClient, StorageSharedKeyCredential, generateBlobSASQueryParameters, BlobSASPermissions} = require("@azure/storage-blob");
+const connectionString = process.env["AzureWebJobsStorage"];
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const id = req.query.id;
+
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
+    const container = "rtrdshr-files";
+    const containerClient = await blobServiceClient.getContainerClient(container);
+
+    let iterator = containerClient.listBlobsFlat({prefix: id});
+    const blob = await iterator.next();
+    context.log(blob);
+
+    const name = blob.value.name;
+    const niceName = name.split('/')[1];
+
+    const blockBlobClient = containerClient.getBlockBlobClient(name);
+    const url = blockBlobClient.url
+
+    const sharedKeyCredential = blobServiceClient.credential;
+    const sasToken = generateBlobSASQueryParameters({
+        containerName: container,
+        blobName: name,
+        expiresOn: new Date(new Date().valueOf() + 60 * 60 * 1000),
+        permissions: BlobSASPermissions.parse("r")
+      }, sharedKeyCredential);
+      
+    const Sasurl = `${blockBlobClient.url}?${sasToken}`;
+    context.log(Sasurl);
+
+    context.res = {
+        status: url ? 200 : 404,
+        body: JSON.stringify({
+            name: niceName,
+            url: Sasurl
+        })
+    };
+}
---
+++ api/uploadFile/index.js
@@ -0,0 +1,40 @@
+var multipart = require("parse-multipart");
+const { BlobServiceClient } = require("@azure/storage-blob");
+const connectionString = process.env["AzureWebJobsStorage"];
+const { v4: uuidv4 } = require('uuid');
+
+async function uploadBlob(file) {
+    // create blobserviceclient object that is used to create container client
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
+    // get reference to a container
+    const container = "rtrdshr-files";
+    const containerClient = await blobServiceClient.getContainerClient(container);
+    // create blob name
+    const key = uuidv4();
+    const blobName = key + '/' + file.filename;
+    // get block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(file.data, file.data.length).catch((err) => {return false})
+    return (uploadBlobResponse == false) ? false : key;
+}
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    context.log(req.headers['content-type'])
+    // get boundary for multipart data 
+    var body = req.body;
+    // get raw body
+    var parts = multipart.Parse(body, boundary);
+
+    var result = await uploadBlob(parts[0])
+
+    context.log(result);
+
+    context.res = {
+        status: result ? 200 : 500,
+        body: result ? JSON.stringify({fileKey: result}) : ''
+    }
+}
+
